9fe016ddcc98f98ef2f0fd8df43152e7aba46a31
Hendricks-3D <odainbrown111@gmail.com>
2025-03-14 21:04:13 -0500

    fix guard test

--- src/Modules/users/auth/auth.guard.spec.ts
+++ src/Modules/users/auth/auth.guard.spec.ts
@@ -1,7 +1,11 @@
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   it('should be defined', () => {
-    expect(new AuthGuard()).toBeDefined();
+    const jwtService = {
+      /* mock implementation of JwtService */
+    } as JwtService;
+    expect(new AuthGuard(jwtService)).toBeDefined();
   });
 });
